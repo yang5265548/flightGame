32789ed65b7e1cd7fc017bedc9e71fca097e6e83
zhiyongzhou <Zhiyong.Zhou@metropolia.fi>
2023-10-05 13:54:49 +0800

    Use file to set database

--- flightGameBeta/public_function/getPropertiesHandler.py
+++ flightGameBeta/public_function/getPropertiesHandler.py
@@ -4,15 +4,15 @@ def getProperties(path):
         with open(path, 'r', encoding='utf-8') as file:
             con= file.readlines();
             # acorrd condition to get a new list
-            content = [item for item in con if "#" not in item and item != '\n']
+            content = [item for item in con if "#" not in item and item != '\n'];
             TempList = [];
             for i in content:
                 TempList.append(tuple(i.strip('\n').split("=")));
                 dataDict = dict(TempList);
             return dataDict;
     except FileNotFoundError as e:
-        print(f"FileNotFoundError: {str(e)}")
+        print(f"FileNotFoundError: {str(e)}");
     except PermissionError as e:
-        print(f"PermissionError: {str(e)}")
+        print(f"PermissionError: {str(e)}");
     except Exception as e:
-        print(f"UnknowError: {str(e)}")
+        print(f"UnknowError: {str(e)}");
