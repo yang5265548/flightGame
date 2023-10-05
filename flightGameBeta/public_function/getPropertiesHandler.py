ce66ddb15e1c110a1531a533988108e9bc2dd59b
zhiyongzhou <Zhiyong.Zhou@metropolia.fi>
2023-10-05 13:53:45 +0800

    Use file to set database Except input setting-data and do not write anything In properties file. Watch out blank!!!

--- flightGameBeta/public_function/getPropertiesHandler.py
+++ flightGameBeta/public_function/getPropertiesHandler.py
@@ -2,11 +2,13 @@
 def getProperties(path):
     try:
         with open(path, 'r', encoding='utf-8') as file:
-            content = file.readlines();
-            list = [];
+            con= file.readlines();
+            # acorrd condition to get a new list
+            content = [item for item in con if "#" not in item and item != '\n']
+            TempList = [];
             for i in content:
-                list.append(tuple(i.split("=")));
-                dataDict = dict(list);
+                TempList.append(tuple(i.strip('\n').split("=")));
+                dataDict = dict(TempList);
             return dataDict;
     except FileNotFoundError as e:
         print(f"FileNotFoundError: {str(e)}")
