4a45e1a4e800807a63f70a7c3573094cabc940d9
zyZ <46803451+zzy1099207684@users.noreply.github.com>
2023-10-08 09:21:11 +0300

    Update DatabaseConnection.py

--- flightGameBeta/public_function/DatabaseConnection.py
+++ flightGameBeta/public_function/DatabaseConnection.py
@@ -2,7 +2,7 @@ import mysql.connector
 import flightGameBeta.public_function.GetPropertiesHandler as p
 import os
 
-# 必须要用os拼路径, 否则路径会找不 到
+# 必须要用os拼路径, 否则路径会找不到1
 path = os.path.join('config','Mysql.properties');
 properties = p.getProperties(path);
 connection = mysql.connector.connect(
